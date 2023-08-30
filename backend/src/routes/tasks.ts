090c553cfb3ae0d2884c6c920528628efde1eb14
Rafael R. Camargo <rafaelrakochinski@gmail.com>
2023-08-30 17:33:52 -0300

    feat(backend): new routes + sample data

---
+++ backend/src/routes/tasks.ts
@@ -0,0 +1,116 @@
+import { Router } from "express";
+
+import type { Task } from "../types/task";
+import { isEmpty } from "../utils";
+import { toTask } from "./utils";
+
+const router = Router();
+
+const tasks: { [key: string]: Task } = {
+  "1": {
+    id: "1",
+    title: "Task 1",
+    description: "Description 1",
+    completed: false,
+    timestamp: new Date().getTime(),
+  },
+  "2": {
+    id: "2",
+    title: "Task 2",
+    description: "Description 2",
+    completed: false,
+    timestamp: new Date().getTime(),
+  },
+};
+
+router.get("/", (_, res) =>
+  /**
+   * Return all tasks.
+   * - Here we morph the object into an array and sort it,
+   * so we can serve it to the client in order.
+   */
+
+  res.json(Object.values(tasks).sort((a, b) => b.timestamp - a.timestamp)),
+);
+
+router.post("/", (req, res) => {
+  const { title, description } = req.body;
+
+  /**
+   * ! Error handling.
+   * - If the title or description is empty, we return a 400 error.
+   * - We also return the missing parameters, so the client can know what went wrong.
+   */
+
+  const { hasEmpty, params } = isEmpty({ title, description });
+
+  if (hasEmpty)
+    return res
+      .status(400)
+      .json({ error: "Required parameters are missing.", params });
+
+  /**
+   * With the error handling out of the way, we can create the task.
+   * - We add a timestamp, so we can sort the tasks by the last created/edited.
+   */
+
+  const task = toTask({ title, description });
+  tasks[task.id] = task;
+
+  return res.json(task);
+});
+
+router.put("/:id", (req, res) => {
+  const { title, description, completed } = req.body;
+  const { id } = req.params;
+
+  /**
+   * ! Error handling.
+   * - If the title or description is empty, we return a 400 error.
+   * - We also return the missing parameters, so the client can know what went wrong.
+   */
+
+  const { hasEmpty, params } = isEmpty({ id, title, description });
+
+  if (hasEmpty)
+    return res
+      .status(400)
+      .json({ error: "Required parameters are missing.", params });
+
+  /**
+   * With the error handling out of the way, we can update the task.
+   * - We update the timestamp, so we can sort the tasks by the last created/edited.
+   * - We also check if the task exists, and return a 404 error if it doesn't.
+   */
+
+  const oldTask = tasks[id];
+
+  if (!oldTask) return res.status(404).json({ error: "Task not found." });
+
+  const newTask = toTask({ id, title, description, completed });
+  tasks[newTask.id] = newTask;
+
+  return res.json(newTask);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  /**
+   * ! Error handling.
+   * - We check if the task exists, and return a 404 error if it doesn't.
+   */
+
+  const task = tasks[id];
+
+  if (!task) return res.status(404).json({ error: "Task not found." });
+
+  /**
+   * With the error handling out of the way, we can delete the task.
+   */
+
+  delete tasks[id];
+  return res.status(204).end();
+});
+
+export default router;
